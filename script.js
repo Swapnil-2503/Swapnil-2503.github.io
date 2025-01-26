b7b78b606ecdeb0fac2fcad86c9a03c73949ba59
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2025-01-26 11:06:15 +0530

    new portflio website

---
+++ script.js
@@ -0,0 +1,3 @@
+// You can add interactivity with JavaScript if needed
+// For now, it's just a placeholder to expand functionality in the future
+console.log("Portfolio website loaded successfully!");
