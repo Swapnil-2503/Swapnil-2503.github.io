17d1b11bcbe2849d11a01f733d2014dcef415efd
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2025-05-07 14:28:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-// You can add interactivity with JavaScript if needed
-// For now, it's just a placeholder to expand functionality in the future
-console.log("Portfolio website loaded successfully!");
